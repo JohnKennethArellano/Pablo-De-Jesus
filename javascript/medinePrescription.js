3723ce4f7e282ed2c02f7016bac2a1958678f3f2
JohnKennethArellano <arellano.johnkenneth.r@gmail.com>
2023-10-28 13:58:50 +0800

    RES

--- javascript/medinePrescription.js
+++ javascript/medinePrescription.js
@@ -6,25 +6,25 @@ function onload() {
 function listeners() {
   const links = document.querySelectorAll(".links");
   links.forEach((link) => {
-      const tooltipText = link.getAttribute("data-tooltip");
+    const tooltipText = link.getAttribute("data-tooltip");
 
-      const tooltip = document.createElement("div");
-      tooltip.classList.add("tooltip");
-      tooltip.textContent = tooltipText;
+    const tooltip = document.createElement("div");
+    tooltip.classList.add("tooltip");
+    tooltip.textContent = tooltipText;
 
-      link.appendChild(tooltip);
+    link.appendChild(tooltip);
   });
-  var profileExpand = document.querySelector('#profileExpand');
-  var logoutHolder = document.querySelector('#logoutHolder');
-  var logoutHolderOptions = document.querySelectorAll('.logoutHolder span');
-  profileExpand.addEventListener("click", ()=>{
+  var profileExpand = document.querySelector("#profileExpand");
+  var logoutHolder = document.querySelector("#logoutHolder");
+  var logoutHolderOptions = document.querySelectorAll(".logoutHolder span");
+  profileExpand.addEventListener("click", () => {
     logoutHolder.classList.toggle("hidden");
-    logoutHolderOptions.forEach((action)=>{
-      action.addEventListener("click", ()=>{
-        logoutHolder.classList.add("hidden")
-      })
-    })
-  })
+    logoutHolderOptions.forEach((action) => {
+      action.addEventListener("click", () => {
+        logoutHolder.classList.add("hidden");
+      });
+    });
+  });
   //search on everytype
   search.addEventListener("input", function () {
     //your code
@@ -111,6 +111,9 @@ function listeners() {
         patient.age;
       clonemedicineTypeTemplate.querySelector("#patientAddress").textContent =
         patient.address;
+      //signature
+      clonemedicineTypeTemplate.querySelector("#doctorSign").textContent =
+        patient.signature;
 
       let patientMedications = "";
 
@@ -159,6 +162,10 @@ function listeners() {
         var address = this.closest(".medicineInformations").querySelector(
           "#patientAddress"
         ).textContent;
+        //signature
+        var signature = this.closest(".medicineInformations").querySelector(
+          "#doctorSign"
+        ).textContent;
 
         printPage.querySelector("#dentistName").textContent = dentistName;
         printPage.querySelector("#patientName").textContent = patientName;
@@ -167,6 +174,7 @@ function listeners() {
           medicinesPrescribed;
         printPage.querySelector("#patientAge").textContent = age;
         printPage.querySelector("#patientAddress").textContent = address;
+        printPage.querySelector("#doctorSignature").src = signature;
       });
 
       var cancelPrintButton = bg.querySelector("#cancelPrint");
@@ -204,14 +212,14 @@ function truncateText(element, limit) {
 }
 function viewNotification(event) {
   var url = event.currentTarget.querySelector("#urlRedirect").textContent;
-  window.location.href ="../Admin" + url;
+  window.location.href = "../Admin" + url;
 }
 
-async function fetchNotification(){
-  const container = document.querySelector("#allNotification")
-  const loader = document.querySelector("#notificationLoader")
-  const mainContainer = document.querySelector("#notificationMainTemplate")
-  const nodatafound = document.querySelector("#no-notifications")
+async function fetchNotification() {
+  const container = document.querySelector("#allNotification");
+  const loader = document.querySelector("#notificationLoader");
+  const mainContainer = document.querySelector("#notificationMainTemplate");
+  const nodatafound = document.querySelector("#no-notifications");
   try {
     for (let i = 0; i < 5; i++) {
       const clone = document.importNode(loader.content, true);
@@ -219,7 +227,7 @@ async function fetchNotification(){
     }
     const response = await fetch("../JSON/notification.json");
     const data = await response.json();
-    console.log( data);
+    console.log(data);
 
     function filterData(data, searchTerm) {
       searchTerm = searchTerm ? searchTerm.toLowerCase() : "";
@@ -228,10 +236,7 @@ async function fetchNotification(){
       for (const notifs of data.notification) {
         const title = notifs.title.toLowerCase();
         const text = notifs.text.toLowerCase();
-        if (
-          title.includes(searchTerm) ||
-          text.includes(searchTerm)
-        ) {
+        if (title.includes(searchTerm) || text.includes(searchTerm)) {
           filteredData.push(notifs);
         }
       }
@@ -246,7 +251,7 @@ async function fetchNotification(){
         const clone = document.importNode(nodatafound.content, true);
         container.appendChild(clone);
       } else {
-        let hasUnreadNotifications = false; 
+        let hasUnreadNotifications = false;
         filteredData.sort((a, b) => {
           if (a.status === b.status) {
             return 0;
@@ -256,14 +261,14 @@ async function fetchNotification(){
         filteredData.forEach((item) => {
           const clone = document.importNode(mainContainer.content, true);
 
-          var notificationStatus = item.status
+          var notificationStatus = item.status;
 
-          if(notificationStatus === false){
-            clone.querySelector(".notif").classList.add("unread")
+          if (notificationStatus === false) {
+            clone.querySelector(".notif").classList.add("unread");
             const notifDot = document.createElement("div");
             notifDot.classList.add("notifDot");
             clone.querySelector(".notif").appendChild(notifDot);
-            hasUnreadNotifications = true; 
+            hasUnreadNotifications = true;
           }
           clone.querySelector("#imgNotif").src = "../images/" + item.image;
           clone.querySelector("#notificationTitle").innerHTML = highlightText(
@@ -279,10 +284,9 @@ async function fetchNotification(){
         });
         const notificationDots = document.querySelector(".notification-dot");
         if (!hasUnreadNotifications) {
-          notificationDots.classList.add("hidden")
-        }
-        else{
-          notificationDots.classList.remove("hidden")
+          notificationDots.classList.add("hidden");
+        } else {
+          notificationDots.classList.remove("hidden");
         }
       }
     }
@@ -292,7 +296,10 @@ async function fetchNotification(){
       }
 
       const regex = new RegExp(`(${escapeRegExp(searchTerm)})`, "gi");
-      return text.replace(regex, (match) => `<p class="highlight">${match}</p>`);
+      return text.replace(
+        regex,
+        (match) => `<p class="highlight">${match}</p>`
+      );
     }
 
     function escapeRegExp(string) {
@@ -304,15 +311,13 @@ async function fetchNotification(){
     search.addEventListener("input", function () {
       updateDisplay(this.value);
     });
-  }
-  catch (error) {
+  } catch (error) {
     console.error("An error occurred:", error);
   }
-
 }
 function viewNotificationContainer(event) {
   var parent = event.target.parentElement;
   var notificationContainer = parent.querySelector(".notificationContainer");
   notificationContainer.classList.toggle("hidden");
-  event.target.classList.toggle("showContainer")
+  event.target.classList.toggle("showContainer");
 }
